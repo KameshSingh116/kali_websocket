dd0eb764b3bcd845151660c9687548914b73d1be
KameshSingh116 <radhakrishnapg152627@gmail.com>
2025-08-08 09:52:11 -0400

    code

---
+++ myserver.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+HOST=''
+PORT=8080
+
+
+mysocket=sockt.socket(socket.AF_INET,socket.SOCK_STREAM)
+print("The socket  has been created!!")
+
+try:
+   mysocket.bind((HOST,PORT))
+except socket.error as msg:
+   print(f"Binding has failed. Error Code is: {str(msg[0])} message: {msg[1]}")
+   sys.exit()
+
+print("Socket binding is complete !, now we can proceed with the socket listening!")\
+
+mysocket.listen(10)
+print("Socket is not listening!!")
+
+while 1:
+      connection,address=mysocket.accept()
+      print(f"Connection with {address[0]} : {str(address[1])}")
+
+mysocket.close()
+
